3036136902a1e2c7f80692b4ed1db2d1bfd9a369
Daniel Mason <daniel@danielmason.com>
2015-03-10 00:56:36 +0000

    Starting angular app

---
+++ public/ayeaye/app.module.js
@@ -0,0 +1,11 @@
+/**
+ * This sets up the application and defines any dependencies
+ * @author Daniel Mason <daniel@loftdigital.com>
+ * @copyright Daniel Mason, 2015
+ */
+
+(function(angular){
+    angular.module(appName, ['ngRoute']);
+
+
+})(angular, appName);
